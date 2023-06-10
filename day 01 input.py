b4d5ee34798894c087abb8420c6e980932b7c2a6
haenii <jints45455@gmail.com>
2023-06-10 22:38:13 +0900

    int, input 변수 지정하는 방법

---
+++ day 01 input.py
@@ -0,0 +1,9 @@
+# input("What is your name?")
+# print(input("What is your name?") + " Hi!")
+# expected value = william Hi! 띄어쓰기 한번.
+#print(input("What is your name?") , " Hi!")
+# ,를 사용하면 띄어쓰기가 포함됨 -> expected value = william  Hi! 띄어쓰기 두번.
+#print("Hi! "+input("What is your name?"))
+#print("Welcome to python!")
+name = input("What is your name?")
+print("Hello! " + name + " Welcome to python.")
