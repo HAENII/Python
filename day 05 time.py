657bfac18a841b7d3d28f8a83462fd26221f474c
haenii <jints45455@gmail.com>
2023-06-16 18:57:27 +0900

    turtle 모듈 진입

--- day 05 time.py
+++ day 05 time.py
@@ -11,11 +11,10 @@ print(f"분: {tm.tm_min}")
 print(f"초: {tm.tm_sec}")
 
 now = time.localtime()
-print(time.strftime('%Y%m%d' , now)) >>> %Y : 네 자리수 연도 // %m : 숫자 월 // %d : 숫자 일
-print(time.strftime('%c', now)) >>> 날짜,요일,시간을 현재 시간대에 맞춰 출력
-print(time.strftime('%x', now)) >>> 현재 시간대 월/일/년도 출력
-print(time.strftime('%X', now)) >>> 현재 시간대 시/분/초 출력
-print(time.strftime('%H%M%S', now)) >>> %H : 시간(24시간) / %M : 분 / %S : 초
+print(time.strftime('%Y%m%d' , now)) #>>> %Y : 네 자리수 연도 // %m : 숫자 월 // %d : 숫자 일
+print(time.strftime('%c', now)) #>>> 날짜,요일,시간을 현재 시간대에 맞춰 출력
+print(time.strftime('%x', now)) #>>> 현재 시간대 시/분/초 출력
+print(time.strftime('%H%M%S', now)) #>>> %H : 시간(24시간) / %M : 분 / %S : 초
 
 
 
